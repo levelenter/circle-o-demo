6d78e457e7047519905a7d533fd6a4fb411d73c6
brickhouse <info@brickhouse.co.jp>
2022-06-29 21:56:52 +0900

    wip

--- src/components/ToTop.vue
+++ src/components/ToTop.vue
@@ -22,9 +22,9 @@ onMounted(() => {
         </a>
       </div>
       <div class="col-auto ml-2">
-        <a href="https://circle-o.jp/app/#/teacher-term-of-use-page?building=co01" target="_blank" class="d-block w-9rem w-sm-13rem w-lg-17rem px-3 px-sm-4 py-2 clr-wht hvr-bg-red hvr-clr-red rounded-xl btn-neumo btn-neumo-co bsd bc-wht bw-02r">
+        <a href="/recruit/" class="d-block w-9rem w-sm-13rem w-lg-17rem px-3 px-sm-4 py-2 clr-wht hvr-bg-red hvr-clr-red rounded-xl btn-neumo btn-neumo-co bsd bc-wht bw-02r">
           <div class="h-100 d-flex flex-wrap align-items-center">
-            <div class="font-V7MG-EB fz-xs fz-md-md fz-lg-lg">先生になる</div>
+            <div class="font-V7MG-EB fz-xs fz-md-md fz-lg-lg">先生大募集</div>
             <div class="ml-auto"><i class="clr-wht fas fa-user-edit"></i> </div>
           </div>
         </a>
