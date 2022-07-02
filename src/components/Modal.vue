839eb38ae593dc4c6e6661e948e59fa098a16a93
9232 <kunimitsu.free@gmail.com>
2022-07-02 16:53:33 +0900

    backface-fixed

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,41 +1,49 @@
 <script setup>
 import {scrollLock} from '@/composables/scrollLock'
+import {backfaceFixed} from '@/utils/backface-fixed'
 
 const props = defineProps({
   id: String,
 })
 import { onMounted } from 'vue'
 
-let scrollTop = 0
+// let scrollTop = 0
 
 onMounted(() => {
   const dialog = document.getElementById(props.id)
   dialog.addEventListener('close', function onClose() {
-    // console.log('close ' + props.id)
-    document.removeEventListener('touchmove', scrollLock)
-    // document.html.classList.remove('is-locked')
-    // document.body.classList.remove('is-locked')
+    // document.removeEventListener('touchmove', scrollLock)
 
-    const scrollElement = document.scrollingElement
-    scrollElement.style.position = ''
-    scrollElement.style.top = ''
-    scrollElement.style.width = ''
-    window.scroll({ top: scrollTop })
+    backfaceFixed(false)
+    // // console.log('close ' + props.id)
+    // document.removeEventListener('touchmove', scrollLock)
+    // // document.html.classList.remove('is-locked')
+    // // document.body.classList.remove('is-locked')
+    //
+    // const scrollElement = document.scrollingElement
+    // console.log(scrollElement)
+    // scrollElement.style.position = ''
+    // scrollElement.style.top = ''
+    // scrollElement.style.width = ''
+    // window.scroll({ top: scrollTop })
   })
 })
 const openModal = () => {
   // console.log('openModal')
   const dialog = document.getElementById(props.id)
   dialog.showModal()
-  document.addEventListener('touchmove', scrollLock, { passive: false })
+
+  backfaceFixed(true)
+
+  // document.addEventListener('touchmove', scrollLock, { passive: false })
   // document.html.classList.add('is-locked')
   // document.body.classList.add('is-locked')
 
-  const scrollElement = document.scrollingElement
-  scrollTop = scrollElement.scrollTop
-  scrollElement.style.position = 'fixed'
-  scrollElement.style.top = -scrollTop + 'px'
-  scrollElement.style.width = '100%'
+  // const scrollElement = document.scrollingElement
+  // scrollTop = scrollElement.scrollTop
+  // scrollElement.style.position = 'fixed'
+  // scrollElement.style.top = -scrollTop + 'px'
+  // scrollElement.style.width = '100%'
 }
 
 const clickDialog = (e) => {
